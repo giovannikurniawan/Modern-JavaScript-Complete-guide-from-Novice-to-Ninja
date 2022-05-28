e3cf036e0c97d3847a7edfecbd4c98a1d1b8c649
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2022-05-28 19:34:41 +0700

    initial commit

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,21 @@
+// print email
+
+let email = "giovanni@umn.ac.id";
+
+console.log(email);
+
+// change email address
+
+email = "giovanni.kurniawan@umn.ac.id";
+
+console.log(email);
+
+// string concatenation
+
+let firstName = "Giovanni";
+let lastName = "Kurniawan";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
