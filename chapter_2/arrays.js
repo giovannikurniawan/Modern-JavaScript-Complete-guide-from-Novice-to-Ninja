f42a2d3b7120a10423c953e385905fc3ad05af78
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2022-06-02 23:30:15 +0700

    masih arrays

--- chapter_2/arrays.js
+++ chapter_2/arrays.js
@@ -1,5 +1,26 @@
 let ninjas = ['shaun', 'ryu', 'chun-li'];
 
-ninjas[1] = 'ken';
-console.log(ninjas);
+// menambah item
+// ninjas[1] = 'ken';
+// console.log(ninjas);
+
+//array method
+
+// let result = ninjas.join('-');
+
+// console.log(result);
+
+// let result = ninjas.indexOf('ryu');
+
+// console.log(result);
+
+// let result = ninjas.concat(['giovanni', 'kurniawan']);
+
+// console.log(result);
+
+let result = ninjas.push('ken');
+
+result = ninjas.pop();
+
+console.log(result);
 
